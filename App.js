4f81be547a81ef8741439b901bdf8657b50296d3
devam-panchasara <devam.p@brucira.com>
2025-06-19 17:47:12 +0530

    update: switch BugTracking import to package and adjust PROJECT_ID and TOKEN values

--- App.js
+++ App.js
@@ -22,13 +22,13 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 import Router from "./src/Router";
 import { CombinedDefaultTheme } from "./src/styles/theme";
-// import { BugTracking } from "@ruttl/bug-tracking";
-import { BugTracking } from "./BugTracking";
+import { BugTracking } from "@ruttl/bug-tracking";
+// import { BugTracking } from "./BugTracking";
 
 const BUILD = true;
-const PROJECT_ID = BUILD ? "XlwddHOIkcWCb3UDdARb" : "yH6Yz68ev0gp24BKcSXp";
+const PROJECT_ID = BUILD ? "oiXLvNlthnCRGofnISkI" : "yH6Yz68ev0gp24BKcSXp";
 const TOKEN = BUILD
-  ? "BD7KAqd0O2hhJcSkoF8N8pBZaKS2"
+  ? "wiorLkSdoxfIH4CJvYWxj78Vou22"
   : "IOj2gf8aP2NuIkOA78xErhXWyJY2";
 
 (async () => {
