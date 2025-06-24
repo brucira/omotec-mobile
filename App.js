6fa7faff9375dcfd934a1fff9df6eb427ba758b6
devam-panchasara <devam.p@brucira.com>
2025-06-24 15:36:17 +0530

    Implement code changes to enhance functionality and improve performance

--- App.js
+++ App.js
@@ -23,10 +23,9 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Router from "./src/Router";
 import { CombinedDefaultTheme } from "./src/styles/theme";
 import { BugTracking } from "@ruttl/bug-tracking";
-// import { BugTracking } from "./BugTracking";
 
 const BUILD = true;
-const PROJECT_ID = BUILD ? "RVofpGNDwdV48GfFODe3" : "yH6Yz68ev0gp24BKcSXp";
+const PROJECT_ID = BUILD ? "TNOrLDyPGnuY1I9om9gI" : "yH6Yz68ev0gp24BKcSXp";
 const TOKEN = BUILD
   ? "wiorLkSdoxfIH4CJvYWxj78Vou22"
   : "IOj2gf8aP2NuIkOA78xErhXWyJY2";
